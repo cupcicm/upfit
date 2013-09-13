c11b679ba4b272adab6f2b2ef7b25d010088ee84
Manu Cupcic <cupcicm@gmail.com>
2013-09-13 23:54:25 +0200

    [Testing] A very simple test email server.
    That we can use for tests.
    It listens on port 2525

---
+++ testing/email.rb
@@ -0,0 +1,17 @@
+require 'mini-smtp-server'
+
+# A MiniSmtpServer that just stores the messages
+# it receives in a list, and allows acces to it.
+class TestMailServer < MiniSmtpServer
+
+  attr_reader :messages
+
+  def initialize
+    super(2525, "127.0.0.1", 1)
+    @messages = Array.new
+  end
+
+  def new_message_event(hash)
+    @messages.push(hash)
+  end
+end
