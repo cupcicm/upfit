6bea42f3806a569ad0a24ccc1633d6f682ca6073
Manu Cupcic <cupcicm@gmail.com>
2013-09-27 08:38:40 +0200

    [Main] Adds a main for upfit.
    This will run a daemon that checks for new files in the watchdir
    and upload them.
    1. It's only strava for now
    2. I should make the configuration a yaml file.

---
+++ main.rb
@@ -0,0 +1,42 @@
+require 'daemons'
+require 'tempfile'
+
+require_relative 'input/listen'
+require_relative 'upload/strava'
+require_relative 'process/types'
+
+
+def main()
+  eval(File.new('config.rb').read())
+  uploader = StravaUploader.new(UpfitConfig[:username],
+                                UpfitConfig[:smtp_options])
+  DirectoryListener.new(UpfitConfig[:watchdir]) do |files|
+    files.each do |file|
+      type = FileTypes.detect(file)
+      if type.nil?
+        puts "Skipping #{file}."
+        next
+      end
+      processor = type.processor
+      if processor.nil?
+        puts "Skipping #{file}"
+        next
+      end
+      ext = File.extname(file)
+      temp = Tempfile.new(['activity', ext])
+      data = File.read(file)
+      data = processor.process(data)
+      temp.write(data)
+      puts "Uploading #{file} to strava..."
+      uploader.upload(file)
+      temp.close
+      temp.unlink
+    end
+  end
+end
+
+if __FILE__ == $0
+  Daemons.run_proc('upfit') do
+    main
+  end
+end
